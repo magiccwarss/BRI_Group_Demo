241cdcdbc1ebb85d8eb7728f2f06c2633004cdee
Narek Khachikyan <112387347+magiccwarss@users.noreply.github.com>
2023-03-29 12:42:37 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navigation_and_others');
 const bodyes = document.querySelector('body');
 
+const faqs = document.querySelectorAll('.FAQ_card');
+const dropDown = document.querySelector('.FAQ_Card_DropDown_Menu');
+const faqText = document.querySelector('.FAQ_cardText');
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   bodyes.classList.toggle('lock');
+});
+
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('drop');
+  });
 });
