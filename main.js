891ad8d8e772c024645168321c263bba3b5411f3
magiccwarss <matem.mayreni@gmail.com>
2023-03-30 12:14:54 +0400

    site_updating

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ faqs.forEach((faq) => {
     faq.classList.toggle('drop');
   });
 });
+
+const links = document.querySelectorAll('a');
+
+for (const link of links) {
+  link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: 'smooth',
+  });
+}
